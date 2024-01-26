945055ae367ea8feabcb2707a6e0ea631772c502
n23020 <n23020@std.it-college.ac.jp>
2024-01-26 01:37:42 +0000

    add layout.js
    p55 ヘッダーとフッターをlayout.jsで管理

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,11 @@
-import Header from 'components/header'
+import Layout from 'components/layout'
 import Hero from 'components/hero'
-import Footer from 'components/footer'
 
 const Home = () => {
   return (
-    <>
-      <Header />
-
-      <main>
-        <div>
-          <Hero />
-        </div>
-      </main>
-
-      <Footer />
-    </>
+    <Layout>
+      <Hero />
+    </Layout>
   )
 }
 export default Home
